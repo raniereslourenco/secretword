8b934fd5b78ced78b6a3716c15587fff1b637399
Raniere Lourenco <raniereslourenco@gmail.com>
2022-08-09 13:56:52 -0300

    SecretWord App

---
+++ src/components/Game.js
@@ -0,0 +1,52 @@
+import './Game.css'
+import { useState, useRef } from 'react'
+
+const Game = ({ verifyLetter, pickedWord, pickedCategory, letters, guessedLetters, wrongLetters, guesses, score }) => {
+
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null); //cria uma referencia
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //impede a atualização da pagna
+    verifyLetter(letter); //recebe a letra digitada
+    setLetter(""); //apaga o campo para receber nova letra
+    letterInputRef.current.focus(); //seta o foco no input e aguarda nova letra
+  };
+
+  return (
+    <div className='game'>
+      <p className='points'>
+        <span>Pontuação: {score}</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className='tip'>
+        Dica sobre a palavra: <span>{pickedCategory}</span>
+      </h3>
+      <p>Você ainda tem {guesses} tentativa(s).</p>
+      <div className="wordContainer">
+        {letters.map((letter,i) => (
+          guessedLetters.includes(letter) ? (
+            <span key={i} className="letter">{letter}</span>
+          ) : (
+            <span key={i} className='blankSquare'></span>
+          )
+        ))}
+      </div>
+      <div className="letterContainer">
+        <p>Tente advinhar uma letra da palavra:</p>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="letter" maxLength="1" required onChange={(e) => setLetter(e.target.value)} value={letter} ref={letterInputRef}/> 
+          <button>Jogar!</button>
+        </form>
+      </div>
+      <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        {wrongLetters.map((letter,i) => (
+          <span key={i}>{letter},</span>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Game
